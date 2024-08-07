dd3ba19e44170eba79165eb7edcacc65f0aeff49
Pedro Eduardo Serafim Roque <pedro.roque@ngpromotora.com.br>
2024-08-07 09:46:36 -0300

    atualização

--- recebimento/recebe.php
+++ recebimento/recebe.php
@@ -3,13 +3,17 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Recebe</title>
 </head>
 <body>
 
 <header>
     <h1>Envio confirmado</h1>
 </header>
+
+<?php 
+var_dump($_GET);
+?>
     
 </body>
 </html>
