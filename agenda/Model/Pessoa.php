6fb49d41630bff35443cfc925d74f29036f7a14f
GiovannaMoyses <giovanna.moyses20@gmail.com>
2024-08-09 11:43:32 -0300

    #main# adicionado atributos a classe pessoa

---
+++ agenda/Model/Pessoa.php
@@ -0,0 +1,22 @@
+<?php 
+
+namespace Comunicacao\Model;
+class Pessoas{
+
+    private $id;
+    private $nome;
+    private $cpf;
+    private $idade;
+    private $apelido;
+
+    function __construct()
+    {
+    $this->id = null;
+    $this->nome = null;
+    $this->cpf = null;
+    $this->idade = null;
+    $this->apelido = null;
+
+    }
+
+}
