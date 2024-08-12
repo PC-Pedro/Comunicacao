344b4611222f6efffbafffe336d21b5bb747047b
Pedro Eduardo Serafim Roque <pedro.roque@ngpromotora.com.br>
2024-08-12 08:03:55 -0300

    #main# ajuste da classe prazo

--- agenda/Model/Prazo.php
+++ agenda/Model/Prazo.php
@@ -19,4 +19,38 @@ class Prazo
         $this->conclusao = null;
         $this->tarefa_id = null;
     }
+
+    function set_id($id)
+    {
+        $this->id = $id;
+    }
+    function get_id()
+    {
+        return $this->id;
+    }
+    function set_hora($hora)
+    {
+        $this->hora = $hora;
+    }
+    function get_hora()
+    {
+        return $this->id;
+    }
+    function set_coclusao($conclusao)
+    {
+        $this->conclusao = $conclusao;
+    }
+    function get_conclusao()
+    {
+        return $this->id;
+    }
+    function set_tarefa_id($tarefa_id)
+    {
+        $this->tarefa_id = $tarefa_id;
+    }
+    function get_tarefa_id()
+    {
+        return $this->id;
+    }
+    
 }
