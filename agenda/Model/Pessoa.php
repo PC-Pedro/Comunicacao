7c3c024b7b16c36729b87bd0b409ffbcde014d2e
GiovannaMoyses <giovanna.moyses20@gmail.com>
2024-08-12 08:12:17 -0300

    #main# adicionado modificacoes a classe pessoa

--- agenda/Model/Pessoa.php
+++ agenda/Model/Pessoa.php
@@ -19,4 +19,45 @@ class Pessoas
         $this->idade = null;
         $this->apelido = null;
     }
+
+    function set_id($id)
+    {
+        $this->$id = $id;
+    }
+    function get_id()
+    {
+        return $this->id;
+    }
+    function set_nome($nome)
+    {
+        $this->$nome = $nome;
+    }
+    function get_nome()
+    {
+        return $this->id;
+    }
+    function set_cpf($cpf)
+    {
+        $this->$cpf = $cpf;
+    }
+    function get_cpf()
+    {
+        return $this->id;
+    }
+    function set_idade($idade)
+    {
+        $this->$idade = $idade;
+    }
+    function get_idade()
+    {
+        return $this->id;
+    }
+    function set_apelido($apelido)
+    {
+        $this->$apelido = $apelido;
+    }
+    function get_apelido()
+    {
+        return $this->id;
+    }
 }
