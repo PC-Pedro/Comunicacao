bba5ce4d607473ce7cf664a8ac6aaa2f33be9c6b
GiovannaMoyses <giovanna.moyses@ngpromotora.com.br>
2024-08-19 10:35:03 -0300

    #main#interface

---
+++ form.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Agenda</title>
+    <link rel="stylesheet" href="agenda.css">
+</head>
+<body>
+<header>
+    <h1>Agenda</h1>
+</header>
+  <selection>
+    <form action="index.php" method="get">
+        <label for="nome">Nome</label>
+        <input type="text" name = "nome" id = "idnome">
+        <label for="cpf">CPF</label>
+        <input type="text" name = "cpf" id = "idcpf">
+        <label for="idade">Idade</label>
+        <input type="number" name="idade" id="ididade">
+        <label for = "apelido">Apelido</label>
+        <input type="text" name="apelido" id="idapelido">
+        <input type="submit" value="Enviar">
+    </form>
+  </selection> 
+</body>
+</html>
+    
+</body>
+</html>
