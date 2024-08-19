f26ca034c72d27e42ab5c2240ac52a5afe009bf4
GiovannaMoyses <giovanna.moyses@ngpromotora.com.br>
2024-08-19 12:04:06 -0300

    #main# tela de agenda

---
+++ agenda.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Agenda</title>
+    <link rel="stylesheet" href="agenda.css">
+</head>
+<body>
+<div>
+<header>
+    <h1>Agenda</h1>
+</header>
+</div>
+<table>
+    <h1>Tarefas<h2>
+
+<th>Tarefas</th>
+<th>Data de inicio</th>
+<th>Data de conclusão</th>
+<th>Prazos</th>
+<th>Horario</th>
+<th>Status</th>
+
+</table>
+
+</body>
+</html>
+    
