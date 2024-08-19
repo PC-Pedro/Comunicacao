83df31e4bf07429ca2b4e3eb1104a7ecb5c730d7
GiovannaMoyses <giovanna.moyses@ngpromotora.com.br>
2024-08-19 15:17:49 -0300

    #main# Atualizações

--- agenda.php
+++ agenda.php
@@ -15,12 +15,12 @@
 <table>
     <h1>Tarefas<h2>
 
-<th>Tarefas</th>
-<th>Data de inicio</th>
-<th>Data de conclusão</th>
-<th>Prazos</th>
-<th>Horario</th>
-<th>Status</th>
+<th>Tarefas<br></th>
+<th>Data de inicio<br></th>
+<th>Data de conclusão<br></th>
+<th>Prazos<br></th>
+<th>Horario<br></th>
+<th>Status<br></th>
 
 </table>
 
