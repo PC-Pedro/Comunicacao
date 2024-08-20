0148d415b2f3876619158ca34fb82a1481764bb1
Pedro Eduardo Serafim Roque <pedro.roque@ngpromotora.com.br>
2024-08-20 09:58:58 -0300

    #main# Adição da fonte para h1

--- View/home/index.php
+++ View/home/index.php
@@ -2,6 +2,28 @@
 include_once '../templates/cabecalho.php'
 ?>
 
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>📒 Agenda</title>
+    <link rel="stylesheet" href="agenda.css">
+</head>
+<body>
+
+<div id="area">
+    <h1>Agenda</h1>
+</div>
+
+<div class="button-container">
+    <a href="../cadastro/form.php">
+        <input type="submit" value="Ir para Cadastro">
+    </a>
+</div>
+    
+</body>
+</html>
 
 
 <?php
