23ba0e6d690a27281816ed648b4d09cba8a2c24c
Pedro Eduardo Serafim Roque <pedro.roque@ngpromotora.com.br>
2024-08-20 10:08:35 -0300

    #main# Correção de erro

--- View/home/index.php
+++ View/home/index.php
@@ -1,5 +1,5 @@
 <?php
-include_once '../templates/cabecalho.php'
+include_once '../templates/cabecalho.php';
 ?>
 
 <!DOCTYPE html>
@@ -27,5 +27,5 @@ include_once '../templates/cabecalho.php'
 
 
 <?php
-include_once '../templates/rodape.php'
+include_once '../templates/rodape.php';
 ?>
