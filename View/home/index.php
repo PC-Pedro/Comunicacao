58cf5b3e6d8cdc7295ddb78df3860f0567ad9476
Pedro Eduardo Serafim Roque <pedro.roque@ngpromotora.com.br>
2024-08-20 10:33:06 -0300

    #main# Correção de erro

--- View/home/index.php
+++ View/home/index.php
@@ -2,16 +2,6 @@
 include_once '../templates/cabecalho.php';
 ?>
 
-<!DOCTYPE html>
-<html lang="pt-br">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>📒 Agenda</title>
-    <link rel="stylesheet" href="agenda.css">
-</head>
-<body>
-
 <div id="area">
     <h1>Agenda</h1>
 </div>
@@ -22,10 +12,6 @@ include_once '../templates/cabecalho.php';
     </a>
 </div>
     
-</body>
-</html>
-
-
 <?php
 include_once '../templates/rodape.php';
 ?>
