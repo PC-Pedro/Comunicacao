26c996dcb67aa318b6c00cc39615710639422052
Pedro Eduardo Serafim Roque <pedro.roque@ngpromotora.com.br>
2024-08-20 11:50:11 -0300

    #main# Atualização

--- View/templates/cabecalho.php
+++ View/templates/cabecalho.php
@@ -1,12 +1,19 @@
-<!DOCTYPE html>
-<html lang="pt-br">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+<?php
+require_once '../../config_serve.php';
+$asset = ASSETS;
+$titulo = TITULO_DO_PROJETO;
+echo "
 
-    <title><?=TITULO_DO_PROJETO ?></title>
-    <link href="<?=ASSETS ?>css/bootstrap.min.css" rel="stylesheet" >
+<!DOCTYPE html>
+<html lang='pt-br'>
+<head>
+    <meta charset='UTF-8'>
+    <meta name='viewport' content='width=device-width, initial-scale=1'>
+
+    <title>$titulo</title>
+    <link href='{$asset}css/bootstrap.min.css' rel='stylesheet'>
 
 </head>
 <body>
-    
+
+";
--- index.php
+++ index.php
@@ -4,4 +4,3 @@ require './vendor/autoload.php';
 require_once './config_serve.php';
 
 // Preciso que inclua o arquivo;
-include_once './View/home/index.php';
