e7a2e2bedac6d2539ba6821f5b6aa88887a0a243
Pedro Eduardo Serafim Roque <pedro.roque@ngpromotora.com.br>
2024-08-21 20:54:34 -0300

    #main# Melhoria na tela principal

--- View/home/index.php
+++ View/home/index.php
@@ -1,36 +1,64 @@
 
 <!-- código php aqui -->
  <?php
-  //adicionar navbar
+
  ?>
 <!-- ./código php aqui -->
 
 
 <!-- código html aqui -->
 
+<img src="./public/assets/imagem/escritorio.jpg" class="full-screen-img">
+
     <div class="row text-center justyfi-aling-center">
         <div class="col-1"></div>
         <div class="col-2">
 
             <div class="button-container">
+              <h1>Agenda</h1>
                 <a href="<?= URL?>/index.php?pg=cadastro-pessoa">
                     <input type="submit" class="btn btn-primary" value="Ir para Cadastro">
                 </a>
             </div>
         </div>
     </div>
+
 <!-- ./código html aqui -->
 
 
 <!-- codigo css customizado aqui -->
  <style>
 
-/*
+h1{
+
+  color: rgb(50, 245, 50);
+  font-size: 100px;
+  position: absolute;
+  top: 40%;
+  left: 40%;
+}
+input[type=submit]{
+
+  background-color: greenyellow;
+  color: black;
+  position: absolute;
+  top: 60%;
+  left: 45%;
+
+}
+
+.full-screen-img {
+    width: 100%;
+    height: 100%;
+    object-fit: cover; /* Faz a imagem cobrir a tela, mantendo a proporção */
+    display: block;
+  }
+
     body{
 
         background-color: rgb(50, 50, 50);
     }
-*/
+
 
  </style>
 <!-- ./codigo css aqui -->
--- View/templates/cabecalho.php
+++ View/templates/cabecalho.php
@@ -26,7 +26,7 @@
           <a class="<?= URL."index.php?pg=cadastro-pessoa"?>" href="#">Features</a>
         </li>
         <li class="nav-item">
-          <a class="<?= URL."index.php?pg=home"?>" href="#">Pricing</a>
+          <a class="<?= URL."index.php?pg=home"?>" href="#">Pricing</a>´
         </li>
         <li class="nav-item dropdown">
           <a class="<?= URL."index.php?pg=home"?>" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
