6a830fa47616f154030be18d58c589e209eb0e26
GiovannaMoyses <giovanna.moyses@ngpromotora.com.br>
2024-08-21 19:14:37 -0300

    #main# implementando interface e códigos ao cadastro pessoas

--- View/cadastro/pessoa/index.php
+++ View/cadastro/pessoa/index.php
@@ -3,20 +3,19 @@
   <h1>Cadastre-se</h1>
 
   Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae, pariatur! Nemo quibusdam, dolore ipsum vel modi distinctio quo, doloremque eveniet voluptatum minima libero accusantium unde. Obcaecati vel cumque amet doloribus.
-
+<div>
   <form>
-  
-    <label for="nome" class="col-sm-2 col-form-label">Nome</label>
-      <input type="Nome" class="form-control" id="Nome">
+    <label for="Nome" class="col-sm-2 col-form-label">Nome</label>
+      <input type="text" class="form-control" id="nome">
     <label for="CPF" class="col-sm-2 col-form-label">CPF</label>
       <input type="text" class="form-control" id="CPF">
-    <label for="Idade" class="col-sm-2 col-form-label">Idade</label>
-      <input type="number" class="form-control" id="Idade">
+      <label for="Idade" class="col-sm-2 col-form-label">Idade</label>
+      <input type="number" class="form-control" id="idade">
       <label for="Apelido" class="col-sm-2 col-form-label">Apelido</label>
-      <input type="number" class="form-control" id="Apelido">
-  <button type="submit" class="btn btn-primary">Enviar</button>
+      <input type="text" class="form-control" id="Apelido">
+  <button type="submit" class="btn btn-primary">Cadastrar</button>
 </form>
-
+</div>
   <style>
 
     body{
@@ -32,17 +31,7 @@ height: 100vh;
     gap: 10px;
 }
 
-div{
 
-background-color: black;
-color: lightgray;
-width: 100%;
-height: 100px;
-display: flex;
-text-align: left;
-padding: 20px;
-font-size: 30px;
-}
 
 
   </style>
