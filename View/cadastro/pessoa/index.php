6fe0808a6103922fdd2b76db0f214005cbc17476
GiovannaMoyses <giovanna.moyses@ngpromotora.com.br>
2024-08-21 10:27:55 -0300

    #main# implementando interface e códigos ao cadastro pessoas

--- View/cadastro/pessoa/index.php
+++ View/cadastro/pessoa/index.php
@@ -1,5 +1,6 @@
-<?php include_once '../templates/cabecalho.php'; ?>
+
   <selection>
+  <h1>Cadastre-se</h1>
 
     <form action="agenda.php" method="get">
         <label for="nome">Nome</label>
@@ -14,4 +15,65 @@
     </form>
   </selection> 
 
-<?php include_once '../templates/rodape.php'; ?>
+  <style>
+
+    body{
+
+margin: 0;
+padding: 0;
+height: 100vh;
+
+}
+
+@font-face {
+    font-family: 'Signika';
+    src: url('../fontes/Signika/Signika-VariableFont_GRAD\,wght.ttf');
+}
+
+h1{
+    font-family: 'Signika', sans-serif;
+}
+
+.button-container {
+    display: flex;
+    gap: 10px;
+}
+
+div{
+
+background-color: black;
+color: lightgray;
+width: 100%;
+height: 100px;
+display: flex;
+text-align: left;
+padding: 20px;
+font-size: 30px;
+}
+
+form {
+    margin-top: 50px;
+    background-color: black;
+    padding: 15px;
+    border-radius: 10px;
+    display: grid;
+    place-items: right;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+}
+
+form label {
+    display: block;
+    width: fit-content;
+    font-size: 0.8em;
+    font-weight: 100;
+    color: black;
+    background-color: lightgray;
+    padding: 3px 7px;
+    margin-top: 10px;
+    margin-bottom: 0px;
+    border-radius: 5px;
+}
+  </style>
