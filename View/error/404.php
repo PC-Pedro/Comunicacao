a9fd528532f62c48e091909463886e3284a9c618
Pedro Eduardo Serafim Roque <pedro.roque@ngpromotora.com.br>
2024-08-22 09:57:40 -0300

    #main# Criação da tela de error 404

--- View/error/404.php
+++ View/error/404.php
@@ -8,12 +8,14 @@
  
  <!-- código html aqui -->
  
-<img src="./public/assets/imagem/agenda_error.png">
+<img src="<?= ASSETS?>imagem/agenda_error.png" class="error-img">
 
 
 <h1 class="h1-not-found">Página não encontrada 😔</h1>
+<h1 class="h1-error-identification">ERROR</h1>
+<h1 class="h1-number-error">404</h1>
 
-<a href="<?= URL."index.php?pg=home"?>">
+<a class="error-button" href="<?= URL."index.php?pg=home"?>">
     <button>Voltar para tela principal</button>
 </a>
  <!-- ./código html aqui -->
@@ -22,18 +24,56 @@
  <!-- codigo css customizado aqui -->
   <style>
 
-    body{
-
-        background-color: rgb(10, 10, 10);
-    }
-
     .h1-not-found{
-      
-        color: red;
+
+      color: red;
+      position: absolute;
+      top: 25%;
+      left: 55%;
+      font-size: 40px;
+
 
     }
 
-    
+    .h1-error-identification{
+
+      color: red;
+      position: absolute;
+      top: 30%;
+      left: 15%;
+      width: 100%;
+      height: 100%;
+      font-size: 100px;
+
+    }
+
+    .h1-number-error{
+
+      color: red;
+      position: absolute;
+      top: 40%;
+      left: 14%;
+      width: 100%;
+      height: 100%;
+      font-size: 180px;
+
+    }
+
+    .error-img{
+
+      position: absolute;
+      top: 30%;
+      left: 57%;
+      width: 20%;
+      height: 50%;
+    }
+
+    .error-button{
+
+      position: absolute;
+      top: 75%; 
+      left: 62%;  
+    }
 
   </style>
  <!-- ./codigo css aqui -->
