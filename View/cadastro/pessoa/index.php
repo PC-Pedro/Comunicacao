57f4e08a7efc23ba319ba1e61d9c576eda4e8aaf
GiovannaMoyses <giovanna.moyses@ngpromotora.com.br>
2024-08-22 11:02:14 -0300

    #main# Arrumando layout pagina cadastro

--- View/cadastro/pessoa/index.php
+++ View/cadastro/pessoa/index.php
@@ -1,84 +1,61 @@
+<selection>
+  <div class="row justify-content-center">
+    <h1 class="text-center mt-2">Cadastre-se</h1>
 
-  <selection>
-    <div id="descricao">
-  <h1>Cadastre-se</h1>
-
-  Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae, pariatur! Nemo quibusdam, dolore ipsum vel modi distinctio quo, doloremque eveniet voluptatum minima libero accusantium unde. Obcaecati vel cumque amet doloribus.
-    </div>
-<div id="form">
-  <form>
-    <label for="Nome" class="col-sm-2 col-form-label"></label>
-      <input type="text" class="form-control" id="nome" placeholder="Nome">
-    <label for="CPF" class="col-sm-2 col-form-label"></label>
-      <input type="text" class="form-control" id="CPF" placeholder="CPF">
-      <label for="Idade" class="col-sm-2 col-form-label"></label>
-      <input type="number" class="form-control" id="idade" placeholder="Idade">
-      <label for="Apelido" class="col-sm-2 col-form-label"></label>
-      <input type="text" class="form-control" id="Apelido" size="50" maxlength="30" placeholder="Apelido">
-  <button type="submit" class="botão">Cadastrar</button>
-</form>
-</div>
-  <style>
-    div#descricao{
-      padding: 10px;
-  position: absolute;
-  top: 10%;
-  right:50%;
-}
-
-div#form{
-  position: absolute;
-  top: 10%;
-  left:60%;
-}
-    form class{ 
-      background-color: rgb(255, 255, 255);
-    padding: 15px;
-    border-radius: 10px;
-}
-
-form label{
-    display: block;
-    width: fit-content;
-    font-size: 1em;
-    font-weight: 100;
-    background-color: rgb(255, 255, 255);
-    padding: 20px px;
-    margin-top: 10px;
-    margin-bottom: 0px;
-    border-radius: 5px;
-}
     
-button {
-  padding: 10px 20px;
-    font-size: 16px;
-    background-color: rgb(5, 238, 5);
-    color: #000000;
-    border: none;
-    float: center;
-    border-radius: 30px;
-    cursor: pointer;
-    text-decoration: none;
-    box-shadow: 0 6px 12px rgba(0, 0, 0, 0.2);
-    transition: backgroud 0.3s, box-shadow 0.3s, transform 0.3s;
-    position: absolute;
-    top:100%;
-    right:85%;
-    transform: translate(-10%, -10%);
+    <div class="col-6" id="descricao">
+
+      <p class="mt-5">ipsum dolor sit amet consectetur adipisicing elit. Vitae, pariatur! Nemo quibusdam, dolore ipsum vel modi distinctio quo, doloremque eveniet voluptatum minima libero accusantium unde. Obcaecati vel cumque amet doloribus.</p>
+    </div>
+    <div class="col-6">
+      <div class="row justify-content-end">
+        <form action="" method="post" class="mt-5">
+          <div class="mb-3">
+            <label for="Nome" class="form-label"></label>
+            <input type="text" class="form-control" id="Nome" placeholder="Nome">
+          </div>
+          <div class="mb-3">
+            <label for="CPF" class="form-label"></label>
+            <input type="text" class="form-control" id="CPF" placeholder="CPF">
+          </div>
+          <div class="mb-3">
+            <label for="Idade" class="form-label"></label>
+            <input type="number" class="form-control" id="Idade" placeholder="Idade">
+          </div>
+          <div class="mb-3">
+            <label for="Apelido" class="form-label"></label>
+            <input type="text" class="form-control" id="Apelido" placeholder="Apelido">
+          </div>
+          <div class="col-6">
+            <button type="submit" class="btn btn-success">Cadastrar</button>
+          </div>
+        </form>
+      </div>
+    </div>
+  </div>
+  <style>
+    h1 {
+      color: white;
     }
 
-    input[type=text], [type=number], select, input[type=date], input[type=date], input[type=datetime], input[type=email], input[type=month], input[type=password], input[type=range], input[type=tel], input[type=time], input[type=week] {
-    width: 100%;
-    padding: 12px 20px;
-    font-size: 1em;
-    margin: 8px 0;
-    display: inline-block;
-    border: 1px solid #ccc;
-    border-radius: 4px;
-    box-sizing: border-box;
-}
+    p {
+      color: white;
+    }
+
+    label {
+      display: block;
+      width: fit-content;
+      font-size: 1em;
+      font-weight: 100;
+      background-color: rgb(255, 255, 255);
+      padding: 20px px;
+      margin-top: 10px;
+      margin-bottom: 0px;
+      border-radius: 5px;
+    }
+
+    
+  
   </style>
 
   <script>
-
-        
