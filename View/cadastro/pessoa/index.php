dcc472b73d6eed6a79923745cd59b8bd589f8488
GiovannaMoyses <giovanna.moyses@ngpromotora.com.br>
2024-08-21 21:55:08 -0300

    #main# implementando interface e códigos ao cadastro pessoas

--- View/cadastro/pessoa/index.php
+++ View/cadastro/pessoa/index.php
@@ -5,35 +5,56 @@
   Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae, pariatur! Nemo quibusdam, dolore ipsum vel modi distinctio quo, doloremque eveniet voluptatum minima libero accusantium unde. Obcaecati vel cumque amet doloribus.
 <div>
   <form>
-    <label for="Nome" class="col-sm-2 col-form-label">Nome</label>
-      <input type="text" class="form-control" id="nome">
-    <label for="CPF" class="col-sm-2 col-form-label">CPF</label>
-      <input type="text" class="form-control" id="CPF">
-      <label for="Idade" class="col-sm-2 col-form-label">Idade</label>
-      <input type="number" class="form-control" id="idade">
-      <label for="Apelido" class="col-sm-2 col-form-label">Apelido</label>
-      <input type="text" class="form-control" id="Apelido">
+    <label for="Nome" class="col-sm-2 col-form-label"></label>
+      <input type="text" class="form-control" id="nome" placeholder="Nome">
+    <label for="CPF" class="col-sm-2 col-form-label"></label>
+      <input type="text" class="form-control" id="CPF" placeholder="CPF">
+      <label for="Idade" class="col-sm-2 col-form-label"></label>
+      <input type="number" class="form-control" id="idade" placeholder="Idade">
+      <label for="Apelido" class="col-sm-2 col-form-label"></label>
+      <input type="text" class="form-control" id="Apelido" placeholder="Apelido">
   <button type="submit" class="btn btn-primary">Cadastrar</button>
 </form>
 </div>
   <style>
 
-    body{
-
-margin: 0;
-padding: 0;
-height: 100vh;
-
+    form{
+      background-color: rgb(255, 255, 255);
+    padding: 15px;
+    border-radius: 10px;
 }
 
-.button-container {
-    display: flex;
-    gap: 10px;
+form label {
+    display: block;
+    width: fit-content;
+    font-size: 0.8em;
+    font-weight: 100;
+    background-color: rgb(255, 255, 255);
+    padding: 3px 7px;
+    margin-top: 10px;
+    margin-bottom: 0px;
+    border-radius: 5px;
+}
+    
+input[type=button], button {
+    padding: 10px 20px;
+    font-size: 16px;
+    background-color: rgb(5, 225,5);
+    color: #000000;
+    border: none;
+    border-radius: 30px;
+    cursor: pointer;
+    text-decoration: none;
+    box-shadow: 0 6px 12px rgba(0, 0, 0, 0.2);
+    transition: backgroud 0.3s, box-shadow 0.3s, transform 0.3s;
+    position: absolute;
+    top: 50%;
+    right:75%;
+    transform: translate(-10%, -10%);
 }
 
 
-
-
+  
   </style>
 
   <script>
